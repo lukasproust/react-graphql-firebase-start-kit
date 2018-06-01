48f216a4e5db7880bebc1df6ef7866d108995264
Lukas Proust <lukas.proust@gmail.com>
2018-06-01 20:57:51 +0200

    fix login layout

--- src/shared/components/Login/index.js
+++ src/shared/components/Login/index.js
@@ -5,8 +5,12 @@ import { Redirect } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import HttpsIcon from '@material-ui/icons/Https';
 import UserContext from 'shared/components/UserContext';
 
 import messages from './intl';
@@ -16,17 +20,23 @@ class Login extends PureComponent {
   state = {
     redirectToReferrer: false,
     isLoading: false,
+    email: undefined,
+    password: undefined,
   };
 
   // TODO use formik && Yup
 
-  login = () => {
-    console.log('login here');
-    // setTimeout(() => {
-    //   fakeAuth.authenticate(() => {
-    //     this.setState({ redirectToReferrer: true, isLoading: false });
-    //   });
-    // }, 300),
+  login = user => {
+    const { email, password } = this.state;
+    console.log('login here', email, password);
+    user.signInWithEmailAndPassword(email, password).catch(error => {
+      // Handle Errors here.
+      console.log(error);
+      // ...
+    });
+    // fakeAuth.authenticate(() => {
+    //   this.setState({ redirectToReferrer: true, isLoading: false });
+    // });
   };
 
   render() {
@@ -48,27 +58,53 @@ class Login extends PureComponent {
               user.currentUser && <Redirect to={fromRoute} />}
             <Card className={css.card}>
               <form>
-                <div className={css.form}>
-                  <div className={css.input}>
-                    <TextField
-                      fullWidth
-                      label={formatMessage(messages.emailLabel)}
-                    />
-                  </div>
-                </div>
-                <div className={css.form}>
-                  <div className={css.input}>
-                    <TextField
-                      fullWidth
-                      label={formatMessage(messages.passwordlHint)}
-                      type="password"
-                    />
-                  </div>
-                </div>
+                <FormControl
+                  aria-describedby="email login"
+                  margin="dense"
+                  fullWidth
+                >
+                  <Grid container spacing={8} alignItems="flex-end">
+                    <Grid item xs={2}>
+                      <AccountCircle />
+                    </Grid>
+                    <Grid item xs={10}>
+                      <TextField
+                        id="email"
+                        label={formatMessage(messages.emailLabel)}
+                        value={this.state.email}
+                        onChange={e => this.setState({ email: e.target.value })}
+                        fullWidth
+                      />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+                <FormControl
+                  aria-describedby="password login"
+                  margin="dense"
+                  fullWidth
+                >
+                  <Grid container fullWidth spacing={8} alignItems="flex-end">
+                    <Grid item xs={2}>
+                      <HttpsIcon />
+                    </Grid>
+                    <Grid item xs={10}>
+                      <TextField
+                        label={formatMessage(messages.passwordlHint)}
+                        value={this.state.password}
+                        type="password"
+                        onChange={e =>
+                          this.setState({ password: e.target.value })
+                        }
+                        fullWidth
+                      />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
                 <CardActions>
                   <Button
                     variant="raised"
-                    onClick={this.login}
+                    onClick={() => this.login(user)}
                     color="primary"
                     fullWidth
                   >
