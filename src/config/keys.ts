8d14d3b4202fbab21d0dccd6d7bcd04fed879ad0
lukas proust <lukas.proust@gmail.com>
2019-07-01 09:51:19 +0200

    fix(F): Update .gitignore

---
+++ src/config/keys.ts
@@ -0,0 +1,7 @@
+/**
+ * This file is ignored by git for the moment until i find the nice way to hide private API keys
+ */
+
+export const PRIVATE_KEYS = {
+  loco: "RuYzDuI7x0-7Z2kEOT7ouBDRTGvjFn705"
+};
